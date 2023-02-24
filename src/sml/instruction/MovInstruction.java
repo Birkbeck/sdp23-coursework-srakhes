fd7a74157aefd71d65fcd1fe11fe79ac994d2d97
Samuel Rakhes <github@samuel.rakh.es>
2023-02-24 19:47:06 +0000

    Updated sml.instrution.MovInstruction equals() and hashcode() with nullsafe equals for label and using Object.hash as per AddInstruction, etc

--- src/sml/instruction/MovInstruction.java
+++ src/sml/instruction/MovInstruction.java
@@ -9,26 +9,30 @@ import java.util.Objects;
 // TODO: write a JavaDoc for the class
 
 /**
+ *
  * @author Birkbeck College, and Samuel Rakhes
  */
 
 public class MovInstruction extends Instruction {
 	private final RegisterName result;
-	private final Integer source;
+	private final int source;
 
 	public static final String OP_CODE = "mov";
 
-	public MovInstruction(String label, RegisterName result, Integer source) {
+	public MovInstruction(String label, RegisterName result, int source) { //TODO:Test to see if runtime error is generator if an int larger than int type can hold.
 		super(label, OP_CODE);
 		this.result = result;
-		this.source = source; //possibly rename this to prevent ambiguity with other register-based variables.
+		this.source = source;
 	}
 
 	@Override
 	public int execute(Machine m) {
 //		int value1 = m.getRegisters().get(result);
 //		int value2 = m.getRegisters().get(source);
+//		m.getRegisters().set(result, value1 * value2);
 		m.getRegisters().set(result, source);
+		// Will throw and ArithmeticException if over/underflow occur
+		// https://docs.oracle.com/en/java/javase/19/docs/api/java.base/java/lang/Math.html#multiplyExact(int,int)
 		return NORMAL_PROGRAM_COUNTER_UPDATE;
 	}
 
@@ -39,17 +43,16 @@ public class MovInstruction extends Instruction {
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o) { return true; } // TODO: this may not be necessary - reread notes and omit if not needed!
 		if (o instanceof MovInstruction that) {
 			return this.result == that.result
 					&& this.source == that.source
-					&& this.opcode == that.opcode;
+					&& Objects.equals(this.label, that.label); // null-safe equals must be used
 		}
 		return false;
 	}
-	//	TODO: Override hashCode
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(this.result, this.source);
+		return Objects.hash(this.result, this.source); //could also multiply result by a prime number
 	}
 }
